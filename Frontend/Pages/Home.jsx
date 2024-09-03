aa22ba21d661826a3ead3bfc2c6443e4d59b2b02
surajaribenchi <surajaribenchi@gmail.com>
2024-09-03 23:15:35 +0530

    change the folder name

---
+++ Frontend/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../Assets/Images/Logo.png'
+const Home = () => {
+  return (
+    <div>
+      <img src={Logo} alt="" />
+    </div>
+  )
+}
+
+export default Home
